5b2098d84a113179781b9f70ca72768d70dfaa57
RodrigoPresa <rodrigo_presa@outlook.com>
2022-06-25 11:05:30 -0300

    Tratando retorno null do método leDados

--- src/view/InterfaceBase.java
+++ src/view/InterfaceBase.java
@@ -1,11 +1,14 @@
 package view;
 
+import controle.Processador;
+
 import javax.swing.*;
 
 public class InterfaceBase {
 
     public static String leDados(String mensagem) throws CampoVazioException {
         String opcao = JOptionPane.showInputDialog(null, mensagem);
+        if(opcao == null) Processador.direcionar("0");
         if (opcao.contains(" ") || opcao.length() == 0) {
             throw new CampoVazioException(mensagem);
         } else {
