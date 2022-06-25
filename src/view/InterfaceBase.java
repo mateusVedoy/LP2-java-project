78062f03fa58cda8173090347ecf2de362de963d
RodrigoPresa <rodrigo_presa@outlook.com>
2022-06-25 11:17:30 -0300

    Ajustando bug de nomes com espaços no método leDados

--- src/view/InterfaceBase.java
+++ src/view/InterfaceBase.java
@@ -9,7 +9,7 @@ public class InterfaceBase {
     public static String leDados(String mensagem) throws CampoVazioException {
         String opcao = JOptionPane.showInputDialog(null, mensagem);
         if(opcao == null) Processador.direcionar("0");
-        if (opcao.contains(" ") || opcao.length() == 0) {
+        if (opcao.equals(" ") || opcao.length() == 0) {
             throw new CampoVazioException(mensagem);
         } else {
             return opcao;
